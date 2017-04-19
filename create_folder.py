8666d28198b7cf587b14781b7b2386d82c154345
Jun Kurata <jun.kurata@canonical.com>
2017-04-19 11:50:27 +0900

    Add files via upload

---
+++ create_folder.py
@@ -0,0 +1,39 @@
+import smartsheet
+
+# Set a talken ID to get your Smartsheet API access
+smartsheet = smartsheet.Smartsheet('3xr8utkze73sz8asjygx4td5fl')
+
+
+# Create folder in "Sheets" folder(Home)
+action = smartsheet.Home.create_folder('PMO-test-folder')
+
+# Set forder Id
+folderId=action.result.id
+
+
+# Create sheet in specific folder 
+sheet = smartsheet.models.Sheet({
+    'name': 'ProjectSchedule',
+    'columns': [{
+            'title': 'Favorite',
+            'type': 'CHECKBOX',
+            'symbol': 'STAR'
+        }, {
+            'title': 'Primary Column',
+            'primary': True,
+            'type': 'TEXT_NUMBER'
+        }, {
+            'title': 'Status',
+            'type': 'PICKLIST',
+            'options': [
+                'Not Started',
+                'Started',
+                'Completed'
+            ]
+        }
+    ]
+})
+
+action = smartsheet.Folders.create_sheet_in_folder(folderId, sheet)
+sheet = action.result
+
