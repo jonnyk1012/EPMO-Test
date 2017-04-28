04da4ae9031329505154962ba01b1e10bc52f990
Jun Kurata <jun.kurata@canonical.com>
2017-04-28 18:31:09 +0900

    Update create_folder.py

--- create_folder.py
+++ create_folder.py
@@ -1,41 +1,63 @@
-import smartsheet
+import sys
 
-# Set a talken ID to get your Smartsheet API access
-smartsheet = smartsheet.Smartsheet('3xr8utkze73sz8asjygx4td5fl')
+def main(talkenid, name):
+    print('Execute main Process')
+    
+    import smartsheet
+
+    # Set a talken ID to get your Smartsheet API access
+    print('Talken ID is',talkenid)
+    print('Project Name is',name)
+    smartsheet = smartsheet.Smartsheet(talkenid)
+
+    # Create folder in "Sheets" folder(Home)
+    action = smartsheet.Home.create_folder(name)
+
+    # Set forder Id
+    folderId=action.result.id
 
 
-# Create folder in "Sheets" folder(Home)
-action = smartsheet.Home.create_folder('PMO-test-folder')
+    # Create sheet in specific folder 
+    sheet = smartsheet.models.Sheet({
+        'name': 'ProjectSchedule',
+        'columns': [{
+                'title': 'Favorite',
+                'type': 'CHECKBOX',
+                'symbol': 'STAR'
+            }, {
+                'title': 'Primary Column',
+                'primary': True,name
+                'type': 'TEXT_NUMBER'
+            }, {
+                'title': 'Status',
+                'type': 'PICKLIST',
+                'options': [
+                    'Not Started',
+                    'Started',
+                    'Completed'
+                ]
+            }
+        ]
+    })
 
-# Set forder Id
-folderId=action.result.id
+    action = smartsheet.Folders.create_sheet_in_folder(folderId, sheet)
+    sheet = action.result
 
-
-# Create sheet in specific folder 
-sheet = smartsheet.models.Sheet({
-    'name': 'ProjectSchedule',
-    'columns': [{
-            'title': 'Favorite',
-            'type': 'CHECKBOX',
-            'symbol': 'STAR'
-        }, {
-            'title': 'Primary Column',
-            'primary': True,
-            'type': 'TEXT_NUMBER'
-        }, {
-            'title': 'Status',
-            'type': 'PICKLIST',
-            'options': [
-                'Not Started',
-                'Started',
-                'Completed'
-            ]
-        }
-    ]
-})
-
-action = smartsheet.Folders.create_sheet_in_folder(folderId, sheet)
-sheet = action.result
-
-print(sheet)
+if __name__ = '__main__':
+    args = sys.argv
+    
+    if len(args) == 3:
+        talkenid = args[1]
+        name     = args[2]
+        main(talkenid, name)
+    else:
+        print('Please specify with following format')
+        print('$ create_folder.py <Talken ID> <Project Name>)
+        quit()
+                  
+        
+        
+        
+        
+        
 
